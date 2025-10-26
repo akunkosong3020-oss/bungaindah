8ed04774c48c68e713a101bb985a5e4dbe59c14c
Afzal <akunkosong3020@gmail.com>
2025-10-26 19:42:39 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `Di buka yaaaa`.split('')
+const text = `Di buka ya jelaa sayang`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
