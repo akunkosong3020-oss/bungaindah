ed1c90dfedc4de6cdc0eb766ecad2fb67995ceaf
Afzal <akunkosong3020@gmail.com>
2025-10-26 19:43:14 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('CANTIK YAA? JELAS DONG ').split('')
+    const titles = ('Bunga ini indah kayak senyum jelaa').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
